cd1a80ca1702b07a1ee5aa5134d8b98bf6c90bff
김나현 <skgus917@gmail.com>
2025-04-21 13:53:41 +0900

    Update CameraRotate.cs

--- Assets/02. Scripts/CameraRotate.cs
+++ Assets/02. Scripts/CameraRotate.cs
@@ -7,6 +7,11 @@ public class CameraRotate : MonoBehaviour
 
     public float RotationSpeed = 120f; // 회전 속도
 
+    // 카메라 각도는 0도에서부터 시작한다고 기준을 세운다.
+    private float _rotationX = 0;
+    private float _rotationY = 0;
+
+
     private void Update()
     {
         // 1. 마우스 입력을 받는다.
@@ -14,18 +19,12 @@ public class CameraRotate : MonoBehaviour
         float mouseY = Input.GetAxis("Mouse Y");
         Debug.Log($"Mouse X: {mouseX}, Mouse Y: {mouseY}");
 
-        // 2. 마우스 입력으로부터 회전시킬 방향을 만든다.
-        // Todo : 마우스 좌표계와 화면 좌표계의 차이점을 알고, 잘 작동 하도록 아래 한줄의 코드를 잘 ~ 수정해보세요.
-        Vector3 dir = new Vector3(-mouseY, mouseX, 0);
+        // 2. 회전한 양만큼 누적시켜 나간다.
+        _rotationX += mouseX * RotationSpeed * Time.deltaTime;
+        _rotationY += -mouseY * RotationSpeed * Time.deltaTime;
+        _rotationY = Mathf.Clamp(_rotationY, -90f, 90f);
 
-        // 3. 카메라를 그 방향으로 회전한다.
-        // 새로운 위치 = 현재 위치 + 속도(속력 * 시간)
-        // 새로운 각도 = 현재 각도 + 회전 속도 * 시간
-        transform.eulerAngles = transform.eulerAngles + dir * RotationSpeed * Time.deltaTime;
-
-        // 회전의 상하 제한이 필요하다! (-90도 ~ +90도)
-        Vector3 rotation = transform.eulerAngles;
-        rotation.x = Mathf.Clamp(rotation.x, -90f, 90f);
-        transform.eulerAngles = rotation;
+        // 3. 회전 방향으로 회전시킨다.
+        transform.eulerAngles = new Vector3(_rotationY, _rotationX, 0);
     }
 }
